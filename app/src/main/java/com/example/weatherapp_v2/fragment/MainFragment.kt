4aaa0b8b73ef4b6c6663e8dd013d2c4db66a3560
Roman Chernega <roman-chernega@yandex.ru>
2022-06-22 20:41:32 +0300

    add days and hours-fragments. create adapter

---
+++ app/src/main/java/com/example/weatherapp_v2/fragment/MainFragment.kt
@@ -0,0 +1,77 @@
+
+package com.example.weatherapp_v2.fragment
+
+import android.Manifest
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.fragment.app.FragmentActivity
+import com.example.weatherapp_v2.R
+import com.example.weatherapp_v2.adapters.ViewPagerAdapter
+import com.example.weatherapp_v2.databinding.FragmentMainBinding
+import com.google.android.material.tabs.TabLayoutMediator
+
+class MainFragment : Fragment() {
+    private val fList = listOf(
+        HoursFragment.newInstance(),
+        DaysFragment.newInstance()
+    )
+    private val tList = listOf(
+        "Hours",
+        "Days"
+    )
+    private lateinit var pLauncher: ActivityResultLauncher<String>
+    private lateinit var binding: FragmentMainBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentMainBinding.inflate(inflater, container, false)
+        // Inflate the layout for this fragment
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        checkPermission()
+        init()
+
+    }
+
+    private fun init() = with(binding) {
+        val adapter = ViewPagerAdapter(activity as FragmentActivity, fList)
+        vPager.adapter = adapter
+        TabLayoutMediator(tabLayout, vPager) {
+            tab, pos -> tab.text = tList[pos]
+        }.attach()
+    }
+
+    private fun permissionListener() {
+        pLauncher = registerForActivityResult(ActivityResultContracts.RequestPermission()){
+            Toast.makeText(activity, "Permission is  $it", Toast.LENGTH_LONG).show()
+        }
+    }
+
+    private fun checkPermission() {
+        if(!isPermissionGranted(Manifest.permission.ACCESS_FINE_LOCATION)) {
+            permissionListener()
+            pLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
+        }
+    }
+
+
+
+
+
+    companion object {
+
+        @JvmStatic
+        fun newInstance() = MainFragment()
+    }
+}
