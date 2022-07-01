574e3568862fc2b19408c6f25d7351de83d6b7ba
Roman Chernega <roman-chernega@yandex.ru>
2022-07-01 17:11:15 +0300

    add cadr-model + WeatherModel.kt

--- app/src/main/java/com/example/weatherapp_v2/fragment/MainFragment.kt
+++ app/src/main/java/com/example/weatherapp_v2/fragment/MainFragment.kt
@@ -10,21 +10,10 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
-import androidx.fragment.app.FragmentActivity
 import com.example.weatherapp_v2.R
-import com.example.weatherapp_v2.adapters.ViewPagerAdapter
 import com.example.weatherapp_v2.databinding.FragmentMainBinding
-import com.google.android.material.tabs.TabLayoutMediator
 
 class MainFragment : Fragment() {
-    private val fList = listOf(
-        HoursFragment.newInstance(),
-        DaysFragment.newInstance()
-    )
-    private val tList = listOf(
-        "Hours",
-        "Days"
-    )
     private lateinit var pLauncher: ActivityResultLauncher<String>
     private lateinit var binding: FragmentMainBinding
 
@@ -40,16 +29,7 @@ class MainFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         checkPermission()
-        init()
 
-    }
-
-    private fun init() = with(binding) {
-        val adapter = ViewPagerAdapter(activity as FragmentActivity, fList)
-        vPager.adapter = adapter
-        TabLayoutMediator(tabLayout, vPager) {
-            tab, pos -> tab.text = tList[pos]
-        }.attach()
     }
 
     private fun permissionListener() {
